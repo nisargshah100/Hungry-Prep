7adb92d9835341997b38e41698cd4d47edf5cee2
Edward Weng <edweng@gmail.com>
2012-05-03 19:36:38 -0400

    Finished OpenID changes

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -26,8 +26,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
       sign_in_and_redirect @user, :event => :authentication
     else
-      session["devise.google_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      redirect_to root_url, :notice => 'User must be using a LivingSocial e-mail to proceed.'
     end
   end
 
