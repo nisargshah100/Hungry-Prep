af25cbcd117f43f8ada43eb94a22f9055a358dad
Chris Maddox <tyre77@gmail.com>
2012-05-03 14:33:39 -0400

    init

---
+++ hungry_prep/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HungryPrep::Application.config.secret_token = 'e3813696d57fccaf6888198d6c58597f6f67261e306b32e52d3176e61b74d906e2b414103a6e9d73ef39e45c3c0aafe83c6194b870fcb5d06e4d27cfba074b39'
