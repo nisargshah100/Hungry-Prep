f51407366b5aeec0aa6f75caadb9a37379f740e8
Daniel Kaufman <daniel.garrett.kaufman@gmail.com>
2012-05-04 10:48:12 -0400

    dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,34 +3,32 @@ class Admin::DashboardController < Admin::AdminController
   end
 
   def show
-    last_statuses =Candidate.all.collect do |candidate|
-      candidate.status
-    end
-    total = last_statuses.count
-    status_hash = Hash.new
+    # last_statuses =Candidate.all.collect do |candidate|
+    #   candidate.status.status
+    # end
+    # total = last_statuses.count
+    # status_hash = Hash.new
+    #
+    # last_statuses.each do |status|
+    #   if status_hash[status]
+    #     status_hash[status] += 1
+    #   else
+    #     status_hash[status] = 1
+    #   end
+    # end
+    #
+    # status_hash.each do |status, count|
+    #   status_hash[status] = ((count.to_f)/(total.to_f)).to_i
+    # end
+    #
+    # # @statuses = [ status_hash["Unfinished"],
+    # #               status_hash["Submitted"],
+    # #               status_hash["Unreviewed"],
+    # #               status_hash["Interviewed"],
+    # #               (status_hash["Accepted"].to_i+status_hash["Declined"].to_i)]
+    #
+    # @statuses = status_hash
 
-    last_statuses.each do |status|
-      if status_hash[status]
-        status_hash[status] += 1
-      else
-        status_hash[status] = 1
-      end
-    end
-
-    status_hash.each do |status, count|
-      status_hash[status] = ((count.to_f)/(total.to_f)).to_i
-    end
-
-    # @statuses = [ status_hash["Unfinished"],
-    #               status_hash["Submitted"],
-    #               status_hash["Unreviewed"],
-    #               status_hash["Interviewed"],
-    #               (status_hash["Accepted"]+status_hash["Declined"])]
-
-    @statuses = Candidate.all
   end
 
 end
-
-
-[ "Unfinished", "Submitted", "Unreviewed", "Interviewed", "Decision Made" ]
