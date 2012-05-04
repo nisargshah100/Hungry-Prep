a0e743f236e136b4a80f938732b7db7ba154eb63
athal7 <andrew.thal@livingocial.com>
2012-05-04 08:38:04 -0400

    require reviewer to see admin stuff

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,3 +1,13 @@
 class Admin::AdminController < ApplicationController
+  before_filter :require_reviewer
   layout 'admin'
+
+  private
+
+  def require_reviewer
+    unless current_user.reviewer
+      flash[:notice] = "Sorry you are not allowed to access that"
+      redirect_to root_path
+    end
+  end
 end
