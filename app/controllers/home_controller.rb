1d73016ab5515ada670b06e529c1885501812225
Chris Coleman <chris.coleman@livingsocial.com>
2012-05-04 07:16:37 -0400

    adding all kinds of front end bullshit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ApplicationController
-  def index
-    if current_user
-      redirect_to edit_candidate_path(current_user.candidate) if current_user.candidate else redirect_to admin_path
-    end
-  end
+	def index
+		redirect_to edit_candidate_path(current_user.candidate) if current_user
+	end
 end
