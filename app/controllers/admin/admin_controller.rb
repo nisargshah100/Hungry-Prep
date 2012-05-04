58f100b236afe79160c4320376d094e3d0da54c0
athal7 <andrew.thal@livingocial.com>
2012-05-04 09:09:59 -0400

    separate sign-in pages

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,11 +1,14 @@
 class Admin::AdminController < ApplicationController
-  before_filter :require_reviewer
+  before_filter :require_reviewer, except: [:login]
   layout 'admin'
+
+  def login
+  end
 
   private
 
   def require_reviewer
-    unless current_user.reviewer
+    unless current_user && current_user.reviewer
       flash[:notice] = "Sorry you are not allowed to access that"
       redirect_to root_path
     end
